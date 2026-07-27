8a042910e2402b6c8ecddcfbebf09a375327e5fd
tom <dev@chabs.name>
2026-07-27 17:25:45 +0200

    chore: nixfmt

--- shell.nix
+++ shell.nix
@@ -1,18 +1,18 @@
 # shell.nix
 let
   nixpkgs = import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-    }) { };  # Import nixpkgs as a package set
+    url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+  }) { }; # Import nixpkgs as a package set
 in
 nixpkgs.mkShell {
   name = "beguelins-dev";
 
   buildInputs = with nixpkgs; [
-      git          # Git for version control
-      go           # Go (required by Hugo)
-      hugo         # Hugo for static website generation
-      dart-sass     # Dart Sass for CSS preprocessing
-    ];
+    git # Git for version control
+    go # Go (required by Hugo)
+    hugo # Hugo for static website generation
+    dart-sass # Dart Sass for CSS preprocessing
+  ];
 
   shellHook = ''
     echo "Welcome to beguelins.net development environment!"
